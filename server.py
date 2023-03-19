6b04a9fc705db7053ae1c4c56bc243275ae78542
Gabriel K <gabrielkklippel@outlook.com>
2023-03-18 20:20:36 -0700

    fixd

--- server.py
+++ server.py
@@ -1,69 +1,42 @@
 import socket
 import select
 
-Header_length=10
-IP="127.0.0.1"
-Port=1337
+HEADER_LENGTH = 10  # Define o tamanho do cabeçalho das mensagens
+IP = "127.0.0.1"  # Define o endereço IP do servidor
+PORT = 1337  # Define a porta do servidor
 
-server_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+# Cria um socket de servidor e define opções de reutilização de endereço
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-server_socket.bind((IP,Port))
+# Vincula o socket do servidor ao endereço IP e à porta definidos
+server_socket.bind((IP, PORT))
+server_socket.listen()  # Coloca o servidor em modo de escuta
 
-server_socket.listen()
-
-sockets_list=[server_socket]
-
-clients = {}
+sockets_list = [server_socket]  # Lista de sockets conectados ao servidor
+clients = {}  # Dicionário para armazenar informações sobre os clientes conectados
 
 def receive_message(client_socket):
-	try:
-		message_header=client_socket.recv(Header_length)
-		if not len(message_header):
-			return False
-		message_length= int(message_header.decode('utf-8').strip())
-		return {"header":message_header,"data":client_socket.recv(message_length)}
-
-	except:
-		return False
+    """
+    Função para receber mensagens de um cliente.
+    """
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)  # Recebe o cabeçalho da mensagem
+        if not len(message_header):  # Se não houver cabeçalho, retorna False
+            return False
+        message_length = int(message_header.decode('utf-8').strip())  # Obtém o comprimento da mensagem a partir do cabeçalho
+        return {"header": message_header, "data": client_socket.recv(message_length)}  # Retorna um dicionário com informações sobre a mensagem recebida
+    except:
+        return False
 
 while True:
-	read_sockets,_,exception_sockets=select.select(sockets_list,[],sockets_list)
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)  # Usa a função select para obter sockets prontos para leitura e exceções
 
-	for notified_socket in read_sockets:
-		if notified_socket == server_socket :
-			client_socket, client_Address= server_socket.accept()
-
-			user=receive_message(client_socket)
-
-			if user is False:
-				continue
-			sockets_list.append(client_socket)
-			clients[client_socket]=user
-			print(clients)
-			print(f"Accepted new connection from {client_Address[0]}:{client_Address[1]} username:{user['data'].decode('utf-8')}")
-
-		else:
-			message = receive_message(notified_socket)
-			if message is False:
-				print(f"closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
-				sockets_list.remove(notified_socket)
-				del clients[notified_socket]
-				continue
-			else:
-				user=clients[notified_socket]
-				print(f"received message from {user['data'].decode('utf-8')}:{message['data'].decode('utf-8')}")
-
-				for client_socket in clients:
-					if client_socket != notified_socket:
-						client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-
-	for notified_socket in exception_sockets:
-		sockets_list.remove(notified_socket)
-		del clients[notified_socket]
-
-
-
-
-
-
+    for notified_socket in read_sockets:  
+        if notified_socket == server_socket: 
+            client_socket, client_address = server_socket.accept() 
+            user = receive_message(client_socket)  
+            if user is False:  
+                continue  
+            sockets_list.append(client_socket) 
+            clients[client_socket]
